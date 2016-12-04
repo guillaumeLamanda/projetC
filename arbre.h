b123a694da3dcba051b707fb5142613a84bdb143
lamanda <guill29640@gmail.com>
2016-12-04 17:55:58 +0100

    Initial commit

---
+++ arbre.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+#include <glib.h>
+// ---------------- ARBRE ------------------------
+
+typedef struct arbre {
+  zpixel_t * zpix[3];
+
+} arbre_t ;
+
+typedef struct noeud {
+  zpixel_t * zpix ;
+  noeud_t * noeudFils[3] ;
+} noeud_t ;
+
+arbre_t * constructArbre(image_t * img);
